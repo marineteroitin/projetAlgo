ba7f69960466bffb01d8c9930b26b96e33bfab1c
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2019-11-28 08:19:08 +0100

    Changement du retour des fonctions d’affichage

--- programmePrincipal.swift
+++ programmePrincipal.swift
@@ -15,8 +15,7 @@ p.joueurCourant = p.commence
 print("Joueur " p.joueurCourant.couleur " à toi de jouer ! \n "
 
 while (!p.finDePartie()) {
-
-	p.afficherPartie() //affiche le plateau avec les pions des 2 joueurs.0
+	print(p.afficherPartie()) //affiche le plateau avec les pions des 2 joueurs sous forme textuelle
 	
  	if(existeDeplacement(p.joueurCourant)){ //vérifie si un déplacement est possible avec les cartes qu'il possède
 
@@ -25,8 +24,8 @@ while (!p.finDePartie()) {
 
 		repeat {
 			//on affiche les pions et les cartes du joueur		
-			p.joueurCourant.afficherCartes()
-			p.joueurCourant.afficherPionsEnVie()
+			print(p.joueurCourant.afficherCartes() )
+			print(p.joueurCourant.afficherPionsEnVie() )
 
 		    	//Le joueur choisis un de ses pions toujours en vie
 			repeat {
@@ -34,7 +33,7 @@ while (!p.finDePartie()) {
 				var x: Int = readLine()
 				var y: Int = readLine() //Le joueur saisi en console la position du pion qu'il veut bouger
 			} while !(p.joueurCourant.existePion(x: x, y: y)) //redemande un pion tant qu'il ne saisie pas la position d'un de ses pions en vie
-			var pionChoisi: TPion = getPion(joueur : p.joueurCourant, position : positionPionChoisi) //récupérer le pion choisi
+			var pionChoisi: TPion = p.joueurCourant.getPion(position: positionPionChoisi) //récupérer le pion choisi
 
 		    	//Le joueur choisis maintenant une carte 
 			repeat {
@@ -52,29 +51,28 @@ while (!p.finDePartie()) {
             			
 			//vérifier que le mouvement que le mouvement choisit est possible avec sa carte et son pion qu'il a choisi
 		    	if (pionChoisi.peutBouger(x: x, y: y)){ 
-					p.joueurCourant.bougerPion(pion: pionChoisi, x: x, y: y) //je tue le pion adverse si besoin lorsque je bouge
+				p.joueurCourant.bougerPion(pion: pionChoisi, x: x, y: y) //je tue le pion adverse si besoin lorsque je bouge
 
 		        	//Une fois que le joueur a bougé son pion: la carte utilisée est échangée avec celle du milieu 
 		        	p.joueurCourant.echangerCarte(carte: carteChoisie, plateau: p)
 				  
-					//le tour est fini
-					finTour = true
-
+				//le tour est fini
+				finTour = true
 		    	} else {
 		            	print("Le mouvement choisi pour ce pion n'est pas possible avec cette carte, il faut choisir une autre carte ou un autre pion \n ")
 			}
 		} while(!finTour)
 	} else {
-        print("Aucun déplacement n'est possible avec les cartes que tu possèdes. Quelle carte veut tu échanger avec celle du plateau ? \n")
+        	print("Aucun déplacement n'est possible avec les cartes que tu possèdes. Quelle carte veut tu échanger avec celle du plateau ? \n")
         	
 		//afficher les carte du joueur
-		p.joueurCourant.afficherCartes()
+		print(p.joueurCourant.afficherCartes() )
 
 		repeat {
 			print("Il faut saisir le nom de la carte à échanger : \n")
 			var nomCarteChoisie: String = readLine() //Le joueur saisi en console le nom de la carte qu'il veut 	
 		} while !p.joueurCourant.existeCarte(nom: nomCarteChoisie) //redemande une carte tant qu'il ne saisie pas le nom d'une de ses cartes
-		var carteChoisie: TCarte = p.joueurCourant.getCarte(nom: nomCarteChoisie ) //récupérer la carte choisie
+		var carteChoisie: TCarte = p.joueurCourant.getCarte(nom: nomCarteChoisie ) //récupérer la carte choisie 
 
     	//echanger les cartes 
 		p.joueurCourant.echangerCarte(carte: carteChoisie, plateau: p)
