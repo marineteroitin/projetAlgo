d721508ac24d97d03604d4e86baf592dc9f70efe
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2019-11-28 08:27:58 +0100

    Changement des fonctions afficher pour retourner un string

--- TCarte.swift
+++ TCarte.swift
@@ -1,28 +1,28 @@
 #encoding: utf-8
 
 //init: -> TCarte
-//Résultat: cette fonction crée une carte avec un nom, une couleur et un motif = l'ensemble des déplacements posibles par rapport à la case occupée par le joueur
-//Post:la couleur est soit "rouge" soit "bleu"
+//Résultat: cette fonction crée une carte avec un nom, une couleur et un motif = l'ensemble des déplacements possibles par rapport à la case occupée par le joueur
+//Pré:la couleur est soit "rouge" soit "bleu"
 init()
 
 //nom : TCarte -> String
 //Résultat: Retourne le nom de la carte
-//Post: La carte passé en paramètre appartient au tas de cartes du plateaux
+//Pré: La carte passé en paramètre appartient au tas de cartes du plateaux
 var nom: String {get}
 
 //couleur : TCarte -> String
 //Résultat: Retourne la couleur de la carte, soit "bleu", soit "rouge"
-//Post: La carte passé en paramètre appartient au tas de cartes du plateaux
+//Pré: La carte passé en paramètre appartient au tas de cartes du plateaux
 var couleur: String {get}
 
 //getMotif: -> (Int,Int)
 //Résultat: retourne tous les déplacements possibles par rapport à la case occupée par le joueur.
-func  getMotif() -> (Int,Int)
+func getMotif() -> (Int,Int)
 
-//afficherCarte : TCarte
-//Resultat: affiche le nom et les déplacements possibles de la carte passée en paramètres
+//afficherCarte : TCarte -> String
+//Resultat: retourne un string qui contient le nom et les déplacements possibles de la carte passée en paramètre
 //Pré: les cartes ont été distribuées
-func afficherCarte() 
+func afficherCarte() -> String
 
 //deplacementAppartientMotif : TCarte x Int x Int -> Bool
 //Résultat: retourne true si le déplacement Int x Int appartient au motif de la carte passée en paramètre
